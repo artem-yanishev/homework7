5b010c35f20eb049453d4ebc87d32a54c4c4ef78
artem-yanishev <janyshevtema79@gmail.com>
2025-04-30 18:21:30 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,24 @@
-class Main2 {
-    public static void main(String[] args) {
-        int salary = 65535;
-        int total = 0;
-        int i = 0;
-        for (; total < 1_000_000; i++) {
-            total = total + total / 100;
-            total = total + salary;
-            if (i % 5 == 0) {
-                System.out.println("Месяц " + i + " Итого " + total);
-            }
-        }
-        System.out.println(total);
-        System.out.println(i);
-        int points = 10;
-
-        do {
-            System.out.println("Осталось " + points + " баллов.");
-            points--;
-        } while (points >= 0);
-
-        System.out.println("Игра окончена!");
-        i = 5;
-        while (i < 9) {
-            System.out.println(i);
-            i++;
-        }
-    }
-}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1.");
         int savings = 15000;
         int total = 0;
-        int i = 0;
+        int month = 0;
         while (total < 2_459_000) {
             total += savings;
-            i++;
-            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей.");
+            month++;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей.");
         }
         System.out.println("Задача 2.");
-        i = 1;
-        while ( i <= 10 ) {
-            System.out.print(i + " ");
-            i++;
+        month = 1;
+        while (month <= 10) {
+            System.out.print(month + " ");
+            month++;
         }
         System.out.println();
-        for (i = 10; i >= 1; ) {
-            System.out.print(i +" ");
-            i--;
+        for (month = 10; month >= 1;) {
+            System.out.print(month + " ");
+            month--;
         }
         System.out.println();
         System.out.println("Задача 3.");
@@ -60,6 +31,60 @@ public class Main {
             System.out.println("Год " + year + ", численность населения составляет " + population);
         }
         System.out.println("Задача 4.");
-
+        double total1 = 15000;
+        month = 0;
+        while (total1 < 12_000_000) {
+            total1 *= 1.07;
+            month++;
+            String formatted = String.format("%.2f", total1);
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + formatted + " рублей.");
+        }
+        System.out.println("Задача 5.");
+        total1 = 15000;
+        month = 0;
+        while (true) {
+            month++;
+            total1 *= 1.07;
+            if (month % 6 == 0) {
+                String formatted = String.format("%.2f", total1);
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + formatted + " рублей.");
+            }
+            if (total1 >= 12_000_000) {
+                if (month % 6 != 0) {
+                    String formatted = String.format("%.2f", total1);
+                    System.out.println("Месяц " + month + ", сумма накоплений равна " + formatted + " рублей.");
+                }
+                break;
+            }
+        }
+        System.out.println("Задача 6.");
+        total1 = 15000;
+        month = 0;
+        int year = 9;
+        while (month < 12 * year) {
+            month++;
+            total1 *= 1.07;
+            if (month % 6 == 0) {
+                String formatted = String.format("%.2f", total1);
+                System.out.println("Месяц " + month + ", сумма накоплений равна " + formatted + " рублей.");
+            }
+        }
+        System.out.println("Задача 7.");
+        int firstFriday = 5;
+        int daysInMonth = 31;
+        if (firstFriday < 1 || firstFriday > 7) {
+            System.out.println("Ошибка: первая пятница должна быть числом от 1 до 7");
+        } else {
+            while (firstFriday <= daysInMonth) {
+                System.out.println("Сегодня пятница, " + firstFriday + "-е число. Необходимо подготовить отчет");
+                firstFriday += 7;
+            }
+        }
+        System.out.println("Задача 8.");
+        int period = 79;
+        for (year = 0;year < 2125; year += period)
+            if (year > 1825) {
+            System.out.println(year);
+        }
     }
 }
